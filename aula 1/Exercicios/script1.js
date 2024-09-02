424692b57d23f518eed4dcccd65f781541f4eb69
Yuji <henryyuji.tr@gmail.com>
2024-09-02 08:51:21 -0300

    Exercícios

---
+++ aula 1/Exercicios/script1.js
@@ -0,0 +1,7 @@
+let numero1 = parseFloat(prompt('Digite um número'))
+let numero2 = parseFloat(prompt('Digite outro número'))
+let numero = (numero1 + numero2)/2
+
+alert('Arredondado: ' + Math.round(numero) + 
+', Arredondado para cima: ' + Math.ceil(numero) + 
+', Arredondado para baixo: ' + Math.floor(numero))
---
+++ aula 1/Exercicios/script2.js
@@ -0,0 +1,4 @@
+let numero = parseInt(prompt('Digite um número para ser elevado'))
+let potencia = parseInt(prompt('Digite um número para ser a potência'))
+
+alert('Resultado: ' + Math.pow(numero, potencia))
