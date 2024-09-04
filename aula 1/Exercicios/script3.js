e7339ddea88001b15928ed8fb9f1447e3a360fb8
Yuji <henryyuji.tr@gmail.com>
2024-09-04 12:59:15 -0300

    Exercicio 3 minimo

--- aula 1/Exercicios/script3.js
+++ aula 1/Exercicios/script3.js
@@ -3,4 +3,5 @@ let num2 = parseFloat(prompt('Digite o segundo número'))
 let num3 = parseFloat(prompt('Digite o terceiro número'))
 let num4 = parseFloat(prompt('Digite o quarto número'))
 
-alert(`O Maior número é o ${Math.max(num1,num2,num3,num4)}`)
+alert(`O Maior número é o ${Math.max(num1,num2,num3,num4)} 
+e o Menor é o ${Math.min(num1,num2,num3,num4)}`)
