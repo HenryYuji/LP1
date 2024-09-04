5c1ec8577f46efa3bb2369c331a7969566630561
Yuji <henryyuji.tr@gmail.com>
2024-09-04 12:41:34 -0300

    Exercicios 3 e 4

---
+++ aula 1/Exercicios/script3.js
@@ -0,0 +1,6 @@
+let num1 = parseFloat(prompt('Digite o primeiro número'))
+let num2 = parseFloat(prompt('Digite o segundo número'))
+let num3 = parseFloat(prompt('Digite o terceiro número'))
+let num4 = parseFloat(prompt('Digite o quarto número'))
+
+alert(`O Maior número é o ${Math.max(num1,num2,num3,num4)}`)
---
+++ aula 1/Exercicios/script4.js
@@ -0,0 +1,3 @@
+let numeroAleatorio = Math.floor(Math.random() * 101)
+
+alert(`Um número aleatório entre 1 e 100 é ${numeroAleatorio}`)
