c760f675bca50269340dc3088a69636078b1288d
Letacio-Bezerra <henryyuji.tr@gmail.com>
2024-09-16 08:58:28 -0300

    update

--- aula 2 - jogo adivinhação/script.js
+++ aula 2 - jogo adivinhação/script.js
@@ -1,11 +1,30 @@
 function gerarNumeroAleatorio(){
-    const numeroAleatorio = Math.floor(Math.random() * 11)
+    const numeroAleatorio = Math.floor(Math.random() * 101)
     return numeroAleatorio
 }
-
 function jogar() {
-    const pegarNumeroAleatorio = gerarNumeroAleatorio()
-    console.log(pegarNumeroAleatorio)
-}
+    const numero = gerarNumeroAleatorio()
+    let palpite = ""
+    let tentativas = 6
+    console.log(numero)
 
+    do {
+       palpite = parseInt(prompt('Adivinhe o número:'))
+       if(palpite < numero) {
+        tentativas--
+            alert(`Número Errado! O numero certo é MAIOR! 
+            Você ainda tem ${tentativas} tentativa(s)!`)
+       }
+       else if(palpite >  numero) {
+        tentativas--
+            alert(`Número Errado! O numero certo é MENOR! 
+            Você ainda tem ${tentativas} tentativa(s)!`)
+       }
+       else if(palpite===numero) {
+            alert('Parabéns! Você acertou!')
+            return
+       }
+    }
+    while(tentativas != 0)
+}
 jogar()
